bc06608ed136cc9f2b5a868d9d1c4fc2b74c0420
Nilton Rocha <jornalista.nilton@gmail.com>
2023-11-24 23:25:08 -0300

    gh pages v0.2

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import styles from './page.module.css'
 
-export async function generateStaticParams() {
-  const posts = await fetch('https://.../').then((res) => res.json())
- 
-  return posts.map((post: any) => ({
-    slug: post.slug,
-  }))
-}
-
-const Blog = ({post}: {post: any}) => {
+const Blog = () => {
   return (
     <div className={styles.container}>
       Blog
